0362ff84843eb17760254fadbd4fe2f7efb0f738
Potmot <377029227@qq.com>
2025-04-25 10:15:13 +0800

    ADD: Background MiniMap

--- src/mindMap/MindMap.vue
+++ src/mindMap/MindMap.vue
@@ -3,12 +3,22 @@ import {VueFlow} from "@vue-flow/core";
 import ContentNode from "@/mindMap/ContentNode.vue";
 import ContentEdge from "@/mindMap/ContentEdge.vue";
 import {MIND_MAP_ID, useMindMap} from "@/mindMap/useMindMap.ts";
+import {MiniMap} from "@vue-flow/minimap";
+import {Background} from "@vue-flow/background";
 
 useMindMap()
 </script>
 
 <template>
-    <VueFlow :id="MIND_MAP_ID" tabindex="-1" style="width: 100%; height: 100%;">
+    <VueFlow
+        :id="MIND_MAP_ID"
+        tabindex="-1"
+        style="width: 100%; height: 100%;"
+        :style="{ backgroundColor: 'var(--background-color)' }"
+    >
+        <Background pattern-color="var(--border-color)"/>
+        <MiniMap pannable zoomable/>
+
         <template #node-CONTENT_NODE="nodeProps">
             <ContentNode v-bind="nodeProps"/>
         </template>
