c4963e251c23e33d960db8e020b64b0d0bd0d4f4
Potmot <377029227@qq.com>
2025-06-21 23:26:30 +0800

    ADD: tsx

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
 import {defineConfig} from "vite";
 import vue from "@vitejs/plugin-vue";
+import vueJsx from '@vitejs/plugin-vue-jsx'
 import {fileURLToPath, URL} from "node:url";
 
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
-    plugins: [vue()],
+    plugins: [
+        vue(),
+        vueJsx(),
+    ],
 
     // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
     //
