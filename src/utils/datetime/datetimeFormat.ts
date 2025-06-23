d4937917a1278970269d876f15ba2aef8ef9fee7
Potmot <377029227@qq.com>
2025-06-23 23:24:46 +0800

    FIX: check date valid

--- src/utils/datetime/datetimeFormat.ts
+++ src/utils/datetime/datetimeFormat.ts
@@ -1,12 +1,18 @@
 const numberReg = /^\d+$/
 
+export const isValidDate = (date: Date) => {
+    return !isNaN(date.getTime())
+}
+
 export const formatDatetimeToLocal = (datetime: string | number | null | undefined) => {
     if (!datetime) return ""
     try {
         if (typeof datetime === "string" && numberReg.test(datetime)) {
             datetime = parseInt(datetime)
         }
-        return new Date(datetime).toLocaleString()
+        const date = new Date(datetime)
+        if (!isValidDate(date)) return datetime
+        return date.toLocaleString()
     } catch (e) {
         return datetime
     }
