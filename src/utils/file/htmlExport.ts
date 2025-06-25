277ef448ccf6ce2d671f30dda165513354dd3658
Potmot <377029227@qq.com>
2025-06-25 23:17:15 +0800

    FIX: exportAsSvg pixedRatio and size

--- src/utils/file/htmlExport.ts
+++ src/utils/file/htmlExport.ts
@@ -2,16 +2,16 @@ import {toJpeg, toPng, toSvg} from 'html-to-image';
 import {downloadImageFile, downloadSvgFile} from "@/utils/file/fileSave.ts";
 
 export const exportAsSvg = async (element: HTMLElement, filename: string) => {
-    const svg = await toSvg(element)
+    const svg = await toSvg(element, {pixelRatio: 2, height: element.offsetHeight, width: element.offsetWidth})
     return await downloadSvgFile(svg, {filename})
 }
 
 export const exportAsPng = async (element: HTMLElement, filename: string) => {
-    const png = await toPng(element)
+    const png = await toPng(element, {pixelRatio: 2, height: element.offsetHeight, width: element.offsetWidth})
     return await downloadImageFile(png, {filename, fileType: "png"})
 }
 
 export const exportAsJpg = async (element: HTMLElement, filename: string) => {
-    const jpg = await toJpeg(element)
+    const jpg = await toJpeg(element, {pixelRatio: 2, height: element.offsetHeight, width: element.offsetWidth})
     return await downloadImageFile(jpg, {filename, fileType: "jpg"})
 }
