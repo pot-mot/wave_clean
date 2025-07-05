73987f522b77f74056c6686933b39704c9b7c6c9
Potmot <377029227@qq.com>
2025-07-06 01:37:49 +0800

    UPDATE: more markdown plugin

--- src/components/markdown/preview/markdownRender.ts
+++ src/components/markdown/preview/markdownRender.ts
@@ -2,14 +2,32 @@ import MarkdownIt from "markdown-it";
 import MermaidIt from "mermaid-it-markdown";
 import "katex/dist/katex.min.css"
 import {MarkdownItKatex} from "@/components/markdown/preview/plugins/MarkdownItKatex.ts";
+//@ts-ignore
+import MarkdownItSub from 'markdown-it-sub';
+//@ts-ignore
+import MarkdownItSup from 'markdown-it-sup';
+//@ts-ignore
+import MarkdownItMark from 'markdown-it-mark';
+//@ts-ignore
+import {full as MarkdownItEmoji} from 'markdown-it-emoji';
+// @ts-ignore
+import MarkdownItTaskLists from 'markdown-it-task-lists';
+import MarkdownItMultiMdTable from 'markdown-it-multimd-table';
 
 const md = new MarkdownIt({
     html: true,
     linkify: true
 })
 
-md.use(MermaidIt)
-md.use(MarkdownItKatex)
+md
+    .use(MarkdownItSub)
+    .use(MarkdownItSup)
+    .use(MarkdownItMark)
+    .use(MarkdownItEmoji)
+    .use(MarkdownItTaskLists)
+    .use(MarkdownItKatex, {strict: false})
+    .use(MarkdownItMultiMdTable, {multiline: true, rowspan: true, headerless: true,})
+    .use(MermaidIt)
 
 export {
     md
