b699a0b6c12b58dd7d58507ea90b6c8e1505bb7d
Potmot <377029227@qq.com>
2025-07-13 22:31:51 +0800

    UPDATE: MessageItem

--- src/components/message/MessageItem.ts
+++ src/components/message/MessageItem.ts
@@ -6,9 +6,13 @@ export type MessageType = "primary" | "success" | "error" | "warning" | "info"
 
 export type MessageItem = {
     id: number,
+    // 消息内容
     content: MessageContent,
+    // 消息类型
     type: MessageType,
+    // 移除计时器
     timeout?: number | undefined,
+    // 是否可关闭
     canClose: boolean,
 }
 
