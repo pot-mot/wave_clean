5f1752a48cbb32941210841fab61ce3df73f37f9
Potmot <377029227@qq.com>
2025-08-17 03:51:24 +0800

    UPDATE: JsonFileOperations noTauriInvokeSubstitution

--- src/utils/file/JsonFileOperations.ts
+++ src/utils/file/JsonFileOperations.ts
@@ -2,46 +2,49 @@ import {
     BaseDirectory,
     create,
     exists,
-    FileHandle,
-    readDir,
     readTextFile,
     remove,
     rename,
     writeTextFile
 } from "@tauri-apps/plugin-fs";
+import {noTauriInvokeSubstitution} from "@/utils/error/noTauriInvokeSubstitution.ts";
 
 export const jsonFileOperations = {
     isExisted: async (key: string) => {
         const path = `${key}.json`
-        return await exists(path, {baseDir: BaseDirectory.AppLocalData})
-    },
-
-    list: async (): Promise<string[]> => {
-        try {
-            const files = await readDir('', {baseDir: BaseDirectory.AppLocalData})
-            return files
-                .filter(file => file.name?.endsWith('.json'))
-                .map(file => file.name!.replace('.json', ''))
-        } catch (e) {
-            console.error(e)
-            throw new Error("list json error")
-        }
+        return await noTauriInvokeSubstitution(async () => {
+            return await exists(path, {baseDir: BaseDirectory.AppLocalData})
+        }, async () => {
+            return localStorage.getItem(path) !== null
+        })
     },
 
     get: async (key: string): Promise<string> => {
         try {
             const path = `${key}.json`
-            return await readTextFile(path, {baseDir: BaseDirectory.AppLocalData})
+            return await noTauriInvokeSubstitution(async () => {
+                return await readTextFile(path, {baseDir: BaseDirectory.AppLocalData})
+            }, async () => {
+                const value = localStorage.getItem(path)
+                if (value === null) {
+                    throw new Error(`json [${path}] not found`)
+                }
+                return value
+            })
         } catch (e) {
             console.error(e)
             throw new Error("get json error")
         }
     },
 
-    create: async (key: string): Promise<FileHandle> => {
+    create: async (key: string): Promise<void> => {
         try {
             const path = `${key}.json`
-            return await create(path, {baseDir: BaseDirectory.AppLocalData})
+            await noTauriInvokeSubstitution(async () => {
+                await create(path, {baseDir: BaseDirectory.AppLocalData})
+            }, async () => {
+                localStorage.setItem(path, "")
+            })
         } catch (e) {
             console.error(e)
             throw new Error("create json error")
@@ -52,9 +55,18 @@ export const jsonFileOperations = {
         try {
             const path = `${key}.json`
             const newPath = `/${newKey}.json`
-            return await rename(path, newPath, {
-                oldPathBaseDir: BaseDirectory.AppLocalData,
-                newPathBaseDir: BaseDirectory.AppLocalData
+            await noTauriInvokeSubstitution(async () => {
+                return await rename(path, newPath, {
+                    oldPathBaseDir: BaseDirectory.AppLocalData,
+                    newPathBaseDir: BaseDirectory.AppLocalData
+                })
+            }, async () => {
+                const value = localStorage.getItem(path)
+                if (value === null) {
+                    throw new Error(`json [${path}] not found`)
+                }
+                localStorage.setItem(newPath, value)
+                localStorage.removeItem(path)
             })
         } catch (e) {
             console.error(e)
@@ -65,7 +77,11 @@ export const jsonFileOperations = {
     set: async (key: string, value: string): Promise<void> => {
         try {
             const path = `${key}.json`
-            return await writeTextFile(path, value, {baseDir: BaseDirectory.AppLocalData})
+            await noTauriInvokeSubstitution(async () => {
+                return await writeTextFile(path, value, {baseDir: BaseDirectory.AppLocalData})
+            }, async () => {
+                localStorage.setItem(path, value)
+            })
         } catch (e) {
             console.error(e)
             throw new Error("set json error")
@@ -75,7 +91,11 @@ export const jsonFileOperations = {
     remove: async (key: string): Promise<void> => {
         try {
             const path = `${key}.json`
-            return await remove(path, {baseDir: BaseDirectory.AppLocalData})
+            await noTauriInvokeSubstitution(async () => {
+                return await remove(path, {baseDir: BaseDirectory.AppLocalData})
+            }, async () => {
+                localStorage.removeItem(path)
+            })
         } catch (e) {
             console.error(e)
             throw new Error("remove json error")
