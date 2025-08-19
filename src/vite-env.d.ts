e41488028c16c2d2eafd09a0566271cb6fcf30a7
Potmot <377029227@qq.com>
2025-08-20 07:20:32 +0800

    UPDATE: vite-plugin-compression

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -5,3 +5,11 @@ declare module "*.vue" {
   const component: DefineComponent<{}, {}, any>;
   export default component;
 }
+
+interface ImportMetaEnv {
+  readonly VITE_RUNTIME: 'browser' | 'tauri'
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,17 @@
-import {defineConfig} from "vite";
+import {defineConfig, PluginOption} from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import {fileURLToPath, URL} from "node:url";
 import {prismjsPlugin} from "vite-plugin-prismjs"
+import compressPlugin from "vite-plugin-compression"
 
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vitejs.dev/config/
-export default defineConfig(async () => ({
-    plugins: [
+export default defineConfig(async (env) => {
+    const shouldCompress = env.mode === "browser";
+
+    const plugins: PluginOption[] = [
         vue(),
         vueJsx(),
         prismjsPlugin({
@@ -48,50 +51,64 @@ export default defineConfig(async () => ({
             'plugins': ['inline-color'],
             css: true
         }),
-    ],
+    ]
 
-    // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
-    //
-    // 1. prevent vite from obscuring rust errors
-    clearScreen: false,
-    // 2. tauri expects a fixed port, fail if that port is not available
-    server: {
+    if (shouldCompress) {
+        plugins.push(compressPlugin({
+            filter: /\.(js|css|json|txt|ico|svg)(\?.*)?$/i, // 需要压缩的文件
+            threshold: 1024, // 文件容量大于这个值进行压缩
+            algorithm: 'gzip', // 压缩方式
+            ext: 'gz', // 后缀名
+            deleteOriginFile: true, // 压缩后是否删除压缩源文件
+        }))
+    }
+
+    return {
+        plugins,
+
+            // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
+            //
+            // 1. prevent vite from obscuring rust errors
+            clearScreen: false,
+        // 2. tauri expects a fixed port, fail if that port is not available
+        server: {
         port: 1420,
-        strictPort: true,
-        host: host || false,
-        hmr: host
+            strictPort: true,
+            host: host || false,
+            hmr: host
             ? {
                 protocol: "ws",
                 host,
                 port: 1421,
             }
             : undefined,
-        watch: {
+            watch: {
             // 3. tell vite to ignore watching `src-tauri`
             ignored: ["**/src-tauri/**"],
         },
     },
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url))
-        }
-    },
-    build: {
-        rollupOptions: {
-            output: {
-                manualChunks: {
-                    'vue': ['vue'],
-                    'vue-flow': ['@vue-flow/core', '@vue-flow/node-toolbar'],
-                    'lodash': ['lodash-es'],
-                    'monaco-editor': ['monaco-editor'],
-                    'prismjs': ['prismjs'],
-                    'markdown-it': ['markdown-it', 'markdown-it-emoji', 'markdown-it-mark', 'markdown-it-multimd-table', 'markdown-it-sub', 'markdown-it-sup', 'markdown-it-task-lists'],
-                    'mermaid': ['mermaid'],
-                    'katex': ['katex'],
-                    'image-viewer': ['v-viewer', 'viewerjs'],
-                    'html-to-image': ['html-to-image'],
+        resolve: {
+            alias: {
+                '@': fileURLToPath(new URL('./src', import.meta.url))
+            }
+        },
+        build: {
+            rollupOptions: {
+                output: {
+                    manualChunks: {
+                        'vue': ['vue'],
+                            'vue-flow': ['@vue-flow/core', '@vue-flow/node-toolbar'],
+                            'lodash': ['lodash-es'],
+                            'monaco-editor': ['monaco-editor'],
+                            'prismjs': ['prismjs'],
+                            'markdown-it': ['markdown-it', 'markdown-it-emoji', 'markdown-it-mark', 'markdown-it-multimd-table', 'markdown-it-sub', 'markdown-it-sup', 'markdown-it-task-lists'],
+                            'mermaid': ['mermaid'],
+                            'katex': ['katex'],
+                            'image-viewer': ['v-viewer', 'viewerjs'],
+                            'html-to-image': ['html-to-image'],
+                    }
                 }
             }
         }
     }
-}));
+});
