2d5c14feb3e36a54e0a1dcc78c07f341d747a78d
Potmot <377029227@qq.com>
2025-08-20 07:55:50 +0800

    UPDATE: min node height to 24

--- src/mindMap/node/ContentNode.ts
+++ src/mindMap/node/ContentNode.ts
@@ -15,7 +15,7 @@ export const ContentNode_Markdown_initWidth = 240
 export const ContentNode_Markdown_initHeight = 160
 
 export const ContentNode_Markdown_minWidth = 24
-export const ContentNode_Markdown_minHeight = 42
+export const ContentNode_Markdown_minHeight = 24
 
 // 内容节点数据
 export type ContentNodeData = {
