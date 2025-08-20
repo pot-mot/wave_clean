92bcac67a65932770cd5b08efe102fd78a2b771a
Potmot <377029227@qq.com>
2025-08-20 23:24:39 +0800

    UPDATE: visualHeight calc

--- src/store/deviceStore.ts
+++ src/store/deviceStore.ts
@@ -4,18 +4,17 @@ import {throttle} from "lodash-es";
 
 export const useDeviceStore = createStore(() => {
     const isTouchDevice = ref('ontouchstart' in window)
-    const visualHeight = ref<number>()
+    const visualHeight = ref(window.innerHeight)
 
     const handleResize = throttle(() => {
         // 通过动态属性修正 vh 在移动端的问题
         if (window.visualViewport) {
-            visualHeight.value = window.visualViewport.height
-            const vh = visualHeight.value * 0.01
-            document.documentElement.style.setProperty('--vh', `${vh}px`)
+            visualHeight.value = Math.min(window.visualViewport.height, window.innerHeight)
         } else {
-            const vh = window.innerHeight * 0.01
-            document.documentElement.style.setProperty('--vh', `${vh}px`)
+            visualHeight.value = window.innerHeight
         }
+        const vh = visualHeight.value * 0.01
+        document.documentElement.style.setProperty('--vh', `${vh}px`)
     }, 200)
 
     onMounted(() => {
