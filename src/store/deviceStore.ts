9cb40a41fe70ba348ae424024fa2f52da2c33a71
Potmot <377029227@qq.com>
2025-08-20 11:53:09 +0800

    FIX: syncVisualHeight merge into handleResize

--- src/store/deviceStore.ts
+++ src/store/deviceStore.ts
@@ -1,32 +1,30 @@
 import {onMounted, onUnmounted, readonly, ref} from "vue";
 import {createStore} from "@/store/createStore.ts";
-import {debounce, throttle} from "lodash-es";
+import {throttle} from "lodash-es";
 
 export const useDeviceStore = createStore(() => {
     const isTouchDevice = ref('ontouchstart' in window)
 
-    const syncVisualHeight = debounce(() => {
-        if (window.visualViewport) {
-            document.documentElement.style.setProperty('--visual-viewport-height', `${window.visualViewport.height}px`)
-        }
-    }, 200)
-
     const handleResize = throttle(() => {
         // 通过动态属性修正 vh 在移动端的问题
-        const vh = window.innerHeight * 0.01
-        document.documentElement.style.setProperty('--vh', `${vh}px`)
+        if (window.visualViewport) {
+            const visualHeight = window.visualViewport.height
+            const vh = visualHeight * 0.01
+            document.documentElement.style.setProperty('--vh', `${vh}px`)
+            document.documentElement.style.setProperty('--visual-viewport-height', `${visualHeight}px`)
+        } else {
+            const vh = window.innerHeight * 0.01
+            document.documentElement.style.setProperty('--vh', `${vh}px`)
+        }
     }, 200)
 
     onMounted(() => {
         handleResize()
-        syncVisualHeight()
         window.addEventListener('resize', handleResize)
-        window.visualViewport?.addEventListener('resize', syncVisualHeight)
     })
 
     onUnmounted(() => {
         window.removeEventListener('resize', handleResize)
-        window.visualViewport?.removeEventListener('resize', syncVisualHeight)
     })
 
     return {
