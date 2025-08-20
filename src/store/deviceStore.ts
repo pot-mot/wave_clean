add8ad6e5cb2cc29c2bec1bd98fd2f5128a3b6ca
Potmot <377029227@qq.com>
2025-08-20 23:17:20 +0800

    REMOVE: visual-viewport-height and html height

--- src/store/deviceStore.ts
+++ src/store/deviceStore.ts
@@ -4,14 +4,14 @@ import {throttle} from "lodash-es";
 
 export const useDeviceStore = createStore(() => {
     const isTouchDevice = ref('ontouchstart' in window)
+    const visualHeight = ref<number>()
 
     const handleResize = throttle(() => {
         // 通过动态属性修正 vh 在移动端的问题
         if (window.visualViewport) {
-            const visualHeight = window.visualViewport.height
-            const vh = visualHeight * 0.01
+            visualHeight.value = window.visualViewport.height
+            const vh = visualHeight.value * 0.01
             document.documentElement.style.setProperty('--vh', `${vh}px`)
-            document.documentElement.style.setProperty('--visual-viewport-height', `${visualHeight}px`)
         } else {
             const vh = window.innerHeight * 0.01
             document.documentElement.style.setProperty('--vh', `${vh}px`)
@@ -29,5 +29,6 @@ export const useDeviceStore = createStore(() => {
 
     return {
         isTouchDevice: readonly(isTouchDevice),
+        visualHeight: readonly(visualHeight),
     }
 })
