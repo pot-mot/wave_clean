9d9aab75dbc528dbbe74356ddde4f5be72c0fd3b
Potmot <377029227@qq.com>
2025-08-25 01:42:39 +0800

    FEAT: ContentNode can both click copy in ContentNode

--- src/mindMap/node/ContentNode.vue
+++ src/mindMap/node/ContentNode.vue
@@ -10,7 +10,6 @@ import {NodeToolbar} from "@vue-flow/node-toolbar";
 import IconDelete from "@/components/icons/IconDelete.vue";
 import IconCopy from "@/components/icons/IconCopy.vue";
 import IconFocus from "@/components/icons/IconFocus.vue";
-import {useDeviceStore} from "@/store/deviceStore.ts";
 import {blurActiveElement, getMatchedElementOrParent} from "@/utils/event/judgeEventTarget.ts";
 import {useMindMapMetaStore} from "@/mindMap/meta/MindMapMetaStore.ts";
 import MarkdownPreview from "@/components/markdown/preview/MarkdownPreview.vue";
@@ -34,8 +33,6 @@ import {
 } from "@/mindMap/node/ContentNode.ts";
 import MarkdownCompositeEditor from "@/components/markdown/compositeEditor/MarkdownCompositeEditor.vue";
 import IconFullScreen from "@/components/icons/IconFullScreen.vue";
-
-const {isTouchDevice} = useDeviceStore()
 
 const {meta} = useMindMapMetaStore()
 
@@ -315,31 +312,29 @@ const handleVisibility = computed<boolean>(() => {
 // 复制
 const executeCopy = () => {
     const node = _node.value
-    if (node !== undefined) {
-        blurActiveElement()
-        copy({nodes: [node] as any as ContentNode[], edges: []}, props.layer)
+    if (node === undefined) return
 
-        // 在复制后的下一次点击中执行粘贴
-        if (isTouchDevice.value) {
-            // 一段时间后移除粘贴动作
-            let timeout = setTimeout(() => {
+    blurActiveElement()
+    copy({nodes: [node] as any as ContentNode[], edges: []}, props.layer)
+
+    // 在复制后的下一次点击中执行粘贴
+    // 一段时间后移除粘贴动作
+    let timeout = setTimeout(() => {
+        document.documentElement.removeEventListener('click', handleNextClick)
+        clearTimeout(timeout)
+    }, 10000)
+    const handleNextClick = (e: MouseEvent) => {
+        if (e.target instanceof HTMLElement) {
+            if (getMatchedElementOrParent(e.target, (el) => el.classList.contains("vue-flow__nodes") || el.classList.contains("vue-flow__edges")) !== null) {
+                return
+            } else if (getMatchedElementOrParent(e.target, (el) => el.classList.contains("vue-flow__pane")) !== null) {
+                paste()
                 document.documentElement.removeEventListener('click', handleNextClick)
                 clearTimeout(timeout)
-            }, 10000)
-            const handleNextClick = (e: MouseEvent) => {
-                if (e.target instanceof HTMLElement) {
-                    if (getMatchedElementOrParent(e.target, (el) => el.classList.contains("vue-flow__nodes") || el.classList.contains("vue-flow__edges")) !== null) {
-                        return
-                    } else if (getMatchedElementOrParent(e.target, (el) => el.classList.contains("vue-flow__pane")) !== null) {
-                        paste()
-                        document.documentElement.removeEventListener('click', handleNextClick)
-                        clearTimeout(timeout)
-                    }
-                }
             }
-            document.documentElement.addEventListener('click', handleNextClick)
         }
     }
+    document.documentElement.addEventListener('click', handleNextClick)
 }
 
 // 边框颜色
